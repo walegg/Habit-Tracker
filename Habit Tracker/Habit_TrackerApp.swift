5aa9d076abe9abb7efe7de676e22b0b17b20f8c1
Louis Mille <mille.louis1@gmail.com>
2023-08-04 16:48:33 +0200

    Initial Commit

---
+++ Habit Tracker/Habit_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_TrackerApp.swift
+//  Habit Tracker
+//
+//  Created by Louis Mille on 04/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Habit_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
